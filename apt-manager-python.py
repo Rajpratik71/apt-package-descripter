4b8ea2ecba2a82aacd923125b0787e613e9671d7
Pratik Raj <Rajpratik71@gmail.com>
2019-06-19 12:06:04 +0530

    Create apt-manager-python.py

---
+++ apt-manager-python.py
@@ -0,0 +1,9 @@
+import apt_pkg
+
+apt_pkg.init()
+mycache = apt_pkg.Cache()
+mydepcache = apt_pkg.DepCache(mycache)
+
+pkgname = input("Enter the package name: ")
+pkg = mydepcache.get_candidate_ver(mycache[pkgname])
+print("The given package version is : ", pkg)
